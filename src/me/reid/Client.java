50c71acfc76582b26aacc250a82d0c0d0b88863c
Reid Campolong <reidcampolong@gmail.com>
2019-01-18 14:39:08 -0500

    Added Sections

--- src/me/reid/Client.java
+++ src/me/reid/Client.java
@@ -1,39 +1,21 @@
 package me.reid;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import me.reid.Section.Section;
 
 public class Client extends Application {
 
+    private static final String VERSION = ".5";
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("TJ Seating by Reid Campolong");
+        primaryStage.setTitle("TJ Seating by Reid Campolong v" + VERSION);
 
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10,10,10,10));
-        gridPane.setVgap(8);
+        Section leftSection = new Section(26, 7);
 
-        Button[] buttons = new Button[10];
-        int x = 0;
-        int y  = 0;
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new Button("A" + i);
-
-            GridPane.setConstraints(buttons[i], x, y);
-            gridPane.getChildren().add(buttons[i]);
-
-            x++;
-            if(x % 3 == 0) {
-                y += 1;
-                x = 0;
-            }
-        }
-
-        Scene scene = new Scene(gridPane,500,500);
+        Scene scene = new Scene(leftSection.getGridPane(), 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/me/reid/Section/Section.java
+++ src/me/reid/Section/Section.java
@@ -1,8 +1,51 @@
 package me.reid.Section;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import me.reid.Utilities.SeatTranslator;
+
 /**
  * A section is a collection of seats with a set row and column count
  */
 public class Section {
+
+    private GridPane gridPane;
+    private Button[][] buttons;
+
+    public Section(int cols, int rows) {
+        gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        this.initPane(cols, rows);
+    }
+
+    private void initPane(int cols, int rows) {
+        buttons = new Button[cols][rows];
+        for (int col = 0; col < buttons.length; col++) {
+            for (int row = 0; row < buttons[col].length; row++) {
+
+                Button button = new Button(SeatTranslator.getName(col, row));
+                button.getProperties().put("pos", new int[]{col, row});
+                buttons[col][row] = button;
+
+                button.setPrefWidth(55);
+                button.setPrefHeight(15);
+                GridPane.setConstraints(button, row, col);
+
+                gridPane.getChildren().add(button);
+                button.setOnMouseClicked(event ->
+                {
+                    int[] properties = (int[]) button.getProperties().get("pos");
+                    System.out.println(SeatTranslator.getName(properties[0], properties[1]));
+                });
+            }
+
+        }
+    }
+
+    public GridPane getGridPane() {
+        return gridPane;
+    }
+
 
 }
